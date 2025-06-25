05a1d2bae8722080c86ff96df0f6bfe8dadb1af1
Mixiro <rizki21ti@mahasiswa.pcr.ac.id>
2025-06-25 11:47:50 +0700

    Update app.py

--- Projek/app.py
+++ Projek/app.py
@@ -7,14 +7,24 @@ import os
 app = Flask(__name__)
 model = load_model('model/bagus.h5')
 
-class_names = ['Bacterial Spot', 'Early Blight', 'Late Blight', 'Healthy','Septoria Leaf Spot','Yellow Leaf Curl Virus'] 
+class_names = ['Bacterial Spot', 'Early Blight', 'Late Blight', 'Healthy', 'Septoria Leaf Spot', 'Yellow Leaf Curl Virus'] 
+
 solutions = {
-    "Bacterial Spot": "Gunakan fungisida berbasis tembaga dan hindari penyiraman dari atas untuk mengurangi kelembaban.",
-    "Early Blight": "Buang daun yang terinfeksi, rotasi tanaman, dan gunakan fungisida berbahan aktif klorotalonil.",
-    "Healthy": "Tanaman sehat, tetap lakukan pemantauan rutin dan pemupukan sesuai jadwal.",
-    "Late Blight": "Cabut dan musnahkan tanaman yang terinfeksi, gunakan fungisida sistemik seperti mankozeb.",
-    "Septoria Leaf Spot": "Buang daun bawah yang terkena, hindari penyiraman berlebih, dan gunakan fungisida.",
-    "Yellow Leaf Curl Virus": "Gunakan insektisida untuk mengendalikan vektor (kutu putih), dan tanam varietas tahan virus."
+    "Bacterial Spot": "Buang sisa tanaman dan bersihkan alat pertanian secara rutin, rendam benih dalam larutan calcium hypochlorite atau air panas, tanam tanaman non-solanaceae selama ≥1 musim tanam, semprotkan fungisida berbasis tembaga + mancozeb.",
+    "Early Blight": "Hancurkan sisa tanaman yang terinfeksi, irigasi tetes: hindari cipratan air ke daun, rotasi tanaman selama 2-3 tahun dengan tanaman bukan famili Solanaceae, fungisida preventif seperti chlorothalonil, azoxystrobin, atau mancozeb.",
+    "Healthy": "Penggunaan benih unggul dan tahan penyakit pilih varietas yang memiliki resistensi genetik terhadap TYLCV, Alternaria, dan Phytophthora, rotasi tanaman hindari menanam tomat di lahan yang sebelumnya digunakan untuk tanaman Solanaceae lainnya (cabai, terong) minimal selama 2 musim, pengaturan jarak tanaman hindari kelembaban berlebih dan memungkinkan sirkulasi udara.",
+    "Late Blight": "Gunakan kultivar seperti ‘Plum Regal’, ‘Mountain Magic’, fungisida sistemik untuk proteksi dan pengobatan, Sanitasi ketat musnahkan tanaman yang terinfeksi dan lakukan solarisasi lahan, Pengaturan kelembaban ventilasi baik dan penanaman dengan jarak cukup.",
+    "Septoria Leaf Spot": "Bersihkan daun sakit dan sisa tanaman, rotasi 1–2 tahun dengan tanaman bukan tomat, jarak tanam dan cagak untuk meningkatkan sirkulasi udara, fungisida chlorothalonil atau mancozeb digunakan secara preventif.",
+    "Yellow Leaf Curl Virus": "Gunakan varietas tahan seperti ‘TY20’, ‘Shanty’ atau ‘TYLCV-tolerant lines', gunakan insektisida sistemik, jaring pelindung & mulsa perak untuk mengurangi serangan vektor, sanitasi dan perangkap lengket kuning kontrol populasi vektor dan tanaman inang alternatif."
+}
+
+symptoms = {
+    "Bacterial Spot": "Bercak air kecil berwarna gelap pada daun yang berkembang menjadi bercak nekrotik, daun menguning, menggulung, dan rontok dini, buah menunjukkan lesi kecil berkerak yang dapat menurunkan kualitas pasar.",
+    "Early Blight": "Bercak coklat tua dengan cincin konsentris seperti target pada daun tua, daun menguning dan rontok mulai dari bawah ke atas, dapat menyerang batang dan buah.",
+    "Healthy": "Daun berwarna hijau cerah, tidak terdapat bercak, tidak melengkung, dan tidak menggulung, batang kokoh dan tidak berwarna coklat/hitam, Bunga dan buah berkembang normal, tidak rontok premature, tidak ada serangan hama seperti kutu putih, ulat daun, atau trips.",
+    "Late Blight": "Bercak air besar berwarna coklat gelap atau kehitaman, bagian bawah daun sering ditumbuhi spora putih di tepinya saat kondisi lembap, infeksi cepat menyebar dan dapat menghancurkan seluruh tanaman.",
+    "Septoria Leaf Spot": "Bercak kecil (1–2 mm), bulat, berwarna kelabu dengan tepi gelap, terdapat titik hitam (pycnidia) di tengah bercak, menyebabkan daun menguning dan gugur dari bawah ke atas.",
+    "Yellow Leaf Curl Virus": "Daun menggulung ke atas, menguning, dan menebal, tanaman kerdil dan pertumbuhan terhambat, infeksi awal dapat menyebabkan gagal berbuah."
 }
 
 @app.route("/", methods=["GET", "POST"])
@@ -31,10 +41,12 @@ def index():
         prediction = model.predict(img_array)
         class_id = np.argmax(prediction)
         label = class_names[class_id]
+        symptom = symptoms.get(label, "Gejala tidak tersedia.")
         solution = solutions.get(label, "Solusi tidak ditemukan.")
+        
 
-        return render_template("result.html", label=label, solution=solution, image_path=path)
-        pass
+        return render_template("result.html", label=label, solution=solution, symptom=symptom, image_path=path)
+
     return render_template("index.html")
 
 if __name__ == '__main__':
